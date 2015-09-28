a873328f1b7d0c8520b04b39b58d50a709191239
Kentaro Fischer <webdev@kentarofischer.com>
2015-09-27 22:28:40 -0700

    Removed gulp watch and live reload.

--- gulpfile.js
+++ gulpfile.js
@@ -2,37 +2,7 @@
 
 var gulp = require( 'gulp' ),
     gutil = require( 'gulp-util' ),
-    livereload = require( 'gulp-livereload' ),
     Server = require( 'karma' ).Server;
-
-
-gulp.task( 'scripts', function () {
-    gulp.src( './js/*.js' )
-        .pipe( livereload() );
-
-    gulp.src( './components/**/*.js' )
-        .pipe( livereload() );
-
-    gulp.src( './components/**/*.jsx' )
-        .pipe( livereload() );
-
-    gulp.src( './*.php' )
-        .pipe( livereload() );
-
-    gulp.src( './views/*.php' )
-        .pipe( livereload() );
-});
-
-
-gulp.task( 'watch', function() {
-    livereload.listen();
-    gulp.watch( 'js/*.js', [ 'scripts' ] );
-    gulp.watch( 'components/**/*.js', [ 'scripts' ] );
-    gulp.watch( 'components/**/*.jsx', [ 'scripts' ] );
-    gulp.watch( '*.php', [ 'scripts' ] );
-    gulp.watch( 'views/*.php', [ 'scripts' ] );
-});
-
 
 // Testing Task
 gulp.task( 'test', function ( done ) {
@@ -44,4 +14,4 @@ gulp.task( 'test', function ( done ) {
 
 
 // Default Task
-gulp.task( 'default', [ 'watch' ] );
+gulp.task( 'default', [ 'test' ] );
