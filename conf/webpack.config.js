71101396a94167b2099790ab91dce07df73e175b
Kentaro Fischer <webdev@kentarofischer.com>
2015-09-29 00:03:45 -0700

    Updates to webpack config.

--- conf/webpack.config.js
+++ conf/webpack.config.js
@@ -1,25 +1,2 @@
-module.exports = function( options ) {
-    var cssLoaders = 'style!css',
-        scssLoaders = cssLoaders + '!sass';
-
-    return {
-        entry: './bundle.js',
-        output: {
-            path: __dirname + '/dist',
-            publicPath: '',
-            filename: 'bundle.js'
-        },
-        module: {
-            loaders: [
-                {
-                    test: /\.css$/,
-                    loaders: cssLoaders
-                },
-                {
-                    test: /\.scss$/,
-                    loaders: scssloaders
-                }
-            ]
-        }
-    };
-}
+module.exports = require('./webpack.make.config')({
+});
---
+++ conf/webpack.make.config.js
@@ -0,0 +1,28 @@
+module.exports = function( options ) {
+    var cssLoaders = 'style!css',
+        scssLoaders = cssLoaders + '!sass';
+
+    return {
+        entry: './js/tinymce-comments-plus.js',
+        output: {
+            path: __dirname + '/js',
+            publicPath: 'http://localhost:8080/',
+            filename: 'bundle.js'
+        },
+        module: {
+            loaders: [
+                {
+                    test: /\.css$/,
+                    loaders: cssLoaders
+                },
+                {
+                    test: /\.scss$/,
+                    loaders: scssLoaders
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['', '.js', '.jsx', '.sass', '.scss', '.css'],
+        }
+    };
+}
