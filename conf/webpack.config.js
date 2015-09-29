f07817f9e49741df8300e398f54848143788e986
Kentaro Fischer <webdev@kentarofischer.com>
2015-09-28 21:22:28 -0700

    Added style, css and sass loaders. Configuration webpack.

--- conf/webpack.config.js
+++ conf/webpack.config.js
@@ -1,12 +1,24 @@
-module.exports = {
-    entry: "./entry.js",
-    output: {
-        path: __dirname,
-        filename: "bundle.js"
-    },
-    module: {
-        loaders: [
-            { test: /\.css$/, loader: "style!css" }
-        ]
-    }
-};
+module.exports = function( options ) {
+    var cssLoaders = 'style!css',
+        scssLoaders = cssLoaders + '!sass';
+
+    return {
+        entry: "./entry.js",
+        output: {
+            path: __dirname + '/dist',
+            filename: "bundle.js"
+        },
+        module: {
+            loaders: [
+                {
+                    test: /\.css$/,
+                    loaders: cssLoaders
+                },
+                {
+                    test: /\.scss$/,
+                    loaders: scssloaders
+                }
+            ]
+        }
+    };
+}
