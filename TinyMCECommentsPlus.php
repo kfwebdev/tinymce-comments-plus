44e3bb77b0b71c91c85110a1ad457e51259c8780
Kentaro Fischer <webdev@kentarofischer.com>
2015-09-29 00:51:18 -0700

    Updated tcp js path to bundle js

--- TinyMCECommentsPlus.php
+++ TinyMCECommentsPlus.php
@@ -289,7 +289,8 @@ class TinyMCECommentsPlus {
 	 */
 	public function enqueue_scripts() {
 
-		wp_enqueue_script( $this->plugin_slug . "-plugin-script", plugins_url( "js/" . $this->plugin_slug . ".js", __FILE__ ), array( 'jquery', 'backbone', 'underscore' ),	$this->version );
+		// wp_enqueue_script( $this->plugin_slug . "-plugin-script", plugins_url( "js/" . $this->plugin_slug . ".js", __FILE__ ), array( 'jquery', 'backbone', 'underscore' ),	$this->version );
+		wp_enqueue_script( $this->plugin_slug . "-plugin-script", "http://localhost:8080/bundle.js", array( 'jquery', 'backbone', 'underscore' ),	$this->version );
 		// Instantiate Javascript Globals for plugin script
 		wp_localize_script( $this->plugin_slug . '-plugin-script', tcp_javascript_globals, json_encode( $this->tcp_plugin_javascript_globals ) );
 	}
