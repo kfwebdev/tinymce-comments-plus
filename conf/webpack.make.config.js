fdb71fe8bf6248795b75de6d9c7cece05137ae34
Kentaro Fischer <webdev@kentarofischer.com>
2015-10-04 22:16:06 -0700

    Fixed typo

--- conf/webpack.make.config.js
+++ conf/webpack.make.config.js
@@ -25,7 +25,6 @@ module.exports = function( options ) {
                     test: /\.js$/,
                     exclude: /node_modules/,
                     loader: babel-loader
-                }
                 },
                 {
                     test: /\.jsx$/,
