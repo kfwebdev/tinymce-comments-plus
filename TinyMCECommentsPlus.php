b9968faeaca57ff7d2c379a97c052b4cde441841
Kentaro Fischer <webdev@kentarofischer.com>
2015-12-31 15:01:24 -0800

    Updated scripts to load file. Ran build.

--- TinyMCECommentsPlus.php
+++ TinyMCECommentsPlus.php
@@ -370,8 +370,8 @@ class TinyMCECommentsPlus {
 
 			wp_enqueue_script( 'jquery-ui-core', array( 'jquery' ) );
 			wp_enqueue_script( 'jquery-ui-spinner', array( 'jquery-ui-core' ) );
-			// wp_register_script( $this->plugin_slug . "-admin-script", plugins_url( "dist/assets/app.js", __FILE__), array( 'jquery', 'backbone' ), $this->version );
-			wp_register_script( $this->plugin_slug . '-admin-script', 'http://localhost:8000/assets/app.js', array( 'jquery', 'backbone', 'underscore' ),	$this->version, false );
+			wp_register_script( $this->plugin_slug . "-admin-script", plugins_url( "dist/assets/app.js", __FILE__), array( 'jquery', 'backbone' ), $this->version );
+			// wp_register_script( $this->plugin_slug . '-admin-script', 'http://localhost:8000/assets/app.js', array( 'jquery', 'backbone', 'underscore' ),	$this->version, false );
 
 			wp_localize_script( $this->plugin_slug . '-admin-script', tcp_javascript_globals, json_encode( $this->tcp_admin_javascript_globals ) );
 			wp_enqueue_script( $this->plugin_slug . '-admin-script' );
@@ -390,8 +390,8 @@ class TinyMCECommentsPlus {
 			// replace comment-reply to handle tinymce form moves
 		  wp_deregister_script( 'comment-reply' );
 
-			// wp_register_script( $this->plugin_slug . "-plugin-script", plugins_url( "dist/assets/app.js", __FILE__ ), array( 'jquery', 'backbone' ),	$this->version, false );
-			wp_register_script( $this->plugin_slug . '-plugin-script', 'http://localhost:8000/assets/app.js', array( 'jquery', 'backbone', 'underscore' ),	$this->version, true );
+			wp_register_script( $this->plugin_slug . "-plugin-script", plugins_url( "dist/assets/app.js", __FILE__ ), array( 'jquery', 'backbone' ),	$this->version, false );
+			// wp_register_script( $this->plugin_slug . '-plugin-script', 'http://localhost:8000/assets/app.js', array( 'jquery', 'backbone', 'underscore' ),	$this->version, true );
 			// Instantiate Javascript Globals for plugin script
 			wp_localize_script( $this->plugin_slug . '-plugin-script', tcp_javascript_globals, json_encode( $this->tcp_plugin_javascript_globals ) );
 			wp_enqueue_script( $this->plugin_slug . '-plugin-script' );
